cfb904100df8aafd1fc75e849992318c84f99ddd
Veeravel-S <veeravel.igt@gmail.com>
2025-12-09 22:48:38 +0530

    added loader and fixed console error

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,11 +2,13 @@ import "@/styles/globals.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import useUTMSource from "@/hooks/useUTMSource";
+import Preloader from "@/common/Preloader";
 
 export default function App({ Component, pageProps }) {
   useUTMSource();
   return (
     <>
+      <Preloader />
       <Component {...pageProps} />
     </>
   );
